b5780ec7d6c17be1f5902aa530a0f084dd228e4c
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-05-08 07:04:58 -0500

    Add files via upload

---
+++ again.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+	"fmt"
+	)
+
+func main(){
+
+  name := "Jeri"
+
+    fmt.Println("Hello World\n",
+		"1 + 1 = ",1 +1,// see this syntax
+		"\nAlso greetings from, ", name)//I need to gts
+				//that math expression is a go
+
+}
+
+
