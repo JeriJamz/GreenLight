b016cc1c6e222512af70b297e9719c65863dbcce
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-05-08 07:18:22 -0500

    Add files via upload

--- again.go
+++ again.go
@@ -12,7 +12,9 @@ func main(){
 		"1 + 1 = ",1 +1,// see this syntax
 		"\nAlso greetings from, ", name)//I need to gts
 				//that math expression is a go
-
+	fmt.Println(len("\nWat are Jooooeeees"),
+			"\nCan I ball " + "Can I chill?")
+	fmt.Println("On my Baby"[1])//idk ill debug when I wake`
 }
 
 
