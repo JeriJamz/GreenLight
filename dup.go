fe82653c141da717dacae4b4e742170149d647f5
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-05-08 19:38:18 -0500

    Add files via upload

---
+++ dup.go
@@ -0,0 +1,32 @@
+//this is supposed to fnd duplicates in stuff
+package main
+
+import("fmt"
+	   "os"
+	   "bufio"//this for input output?
+)
+
+func main(){
+
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan(){
+
+		counts[input.Text()++]
+
+	}
+
+	for line, n := range counts{
+
+		if n > 1 {
+
+			fmt.Printf("%d\t%s\n", n, line)
+
+		}
+
+	}
+
+}
+
+
+
