53edf6ea7515d34d4fee827c13d51da30bb562d3
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-08-07 12:51:27 -0500

    Add files via upload

---
+++ sumTypeOFserver.go
@@ -0,0 +1,69 @@
+import("fmt"
+       "os"
+       "net"
+       "net/http"
+       "sync"
+       "log"
+       "encoding/json"
+       "encoding/gob")
+
+
+
+type Usr struct{
+
+    UsrName UsrName
+    Password Password
+    Email []Email
+
+}
+
+
+type UsrName struct{
+
+    usrName string
+
+}
+
+type Password struct{
+
+    Password string
+
+}
+
+type Email struct{
+
+    Address string
+
+}
+
+func main(){
+
+
+
+}
+
+func NewUsrLogin(fileName, key interface){
+
+    fmt.Println("Hello New User. Please Type in your User Name")
+    
+
+    outFile, err := os.Create(fileName)
+    checkError(err)
+    encoder := gob.NewEncoder(outFile)
+    err = encoder.Encode(key)
+    checkError(err)
+    outFile.Close()
+
+}
+
+func checkError(err error){
+
+    if err != nil{
+
+	fmt.Println("Something went wrong")
+	os.Exit(1)
+
+    }
+
+}
+
